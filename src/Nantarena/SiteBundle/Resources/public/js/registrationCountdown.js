f77940a1d0160adde901050759ba9992d448c3b9
Julien Barret <ibarretj@gmail.com>
2014-09-25 13:06:13 +0200

    Countdown before registration - JS version.

---
+++ src/Nantarena/SiteBundle/Resources/public/js/registrationCountdown.js
@@ -0,0 +1,36 @@
+$(function(){
+	// On récupère l'élément 'note' dans lequel on va inscrire des informations
+	var tournamentProgress = document.getElementById('tournamentProgress');
+	var tournamentCountdown = document.getElementById('tournamentCountdown');
+	// Création de l'objet 'date' (année / mois / jour)
+	// Attention les mois commencent à 0 !
+	var ts = new Date(2014, 09, 1);
+	
+	if((new Date()) > ts){
+		// L'évènement est passé, on affiche les inscriptions et on cache le compteur
+		tournamentCountdown.style.display = 'none';
+	}
+	else {
+	
+		// On cache la progression des inscriptions et on forme le compteur
+		tournamentProgress.style.display = 'none';	
+		$('#countdown').countdown({
+			timestamp	: ts,
+			callback	: function(days, hours, minutes, seconds){
+				var message = "";
+				message += "<center>";
+				message += "<p>Les inscriptions sont actuellement fermées.</p><p>Ouverture des inscriptions dans...</p>";
+				message += days + " jour" + ( days == 1 ? '':'s' ) + ", ";
+				message += hours + " heure" + ( hours==1 ? '':'s' ) + ", ";
+				message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " et ";
+				message += seconds + " seconde" + ( seconds==1 ? '':'s' ); 
+				message += "</center>";
+							
+				
+				$('#tournamentCountdown').html(message);
+			}
+		});
+
+	}	
+	
+});
