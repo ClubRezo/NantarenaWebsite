e0ed7835f9bf51e0a3de5987486ffe0c61286407
GUIHAL Maxime <sgp.jaconil@gmail.com>
2014-10-02 02:05:14 +0200

    Fix countdown display

--- src/Nantarena/SiteBundle/Resources/public/js/registrationCountdown.js
+++ src/Nantarena/SiteBundle/Resources/public/js/registrationCountdown.js
@@ -1,20 +1,16 @@
 $(function(){
-    // On récupère l'élément 'note' dans lequel on va inscrire des informations
     var tournamentProgress = $('#tournamentProgress');
     var tournamentCountdown = $('#tournamentCountdown');
-    // Création de l'objet 'date' (année / mois / jour)
-    // Attention les mois commencent à 0 !
+
     var ts = new Date(2014, 09, 15);
 
     if((new Date()) > ts){
-        // L'évènement est passé, on affiche les inscriptions et on cache le compteur
         tournamentCountdown.css('display', 'none');
     }
     else {
 
-        // On cache la progression des inscriptions et on forme le compteur
         tournamentProgress.css('display', 'none');
-        $('#countdown').countdown({
+        $('div.countdown').countdown({
             timestamp	: ts,
             callback	: function(days, hours, minutes, seconds){
                 var message = "";
