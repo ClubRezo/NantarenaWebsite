f9daac8ab60dbcb1aa999101171e750666b2e271
GUIHAL Maxime <sgp.jaconil@gmail.com>
2014-10-03 20:43:47 +0200

    Fix countdown display

--- src/Nantarena/SiteBundle/Resources/public/js/registrationCountdown.js
+++ src/Nantarena/SiteBundle/Resources/public/js/registrationCountdown.js
@@ -1,32 +1,14 @@
 $(function(){
-    var tournamentProgress = $('#tournamentProgress');
-    var tournamentCountdown = $('#tournamentCountdown');
+    $('div.countdown').each(function() {
+        var deadline = $(this).text();
+        var ts = new Date(deadline);
 
-    var ts = new Date(2014, 09, 15);
+        $(this).text('');
 
-    if((new Date()) > ts){
-        tournamentCountdown.css('display', 'none');
-    }
-    else {
-
-        tournamentProgress.css('display', 'none');
-        $('div.countdown').countdown({
-            timestamp	: ts,
-            callback	: function(days, hours, minutes, seconds){
-                var message = "";
-                message += "<center>";
-                message += "<p>Les inscriptions sont actuellement fermées.</p><p>Ouverture des inscriptions dans...</p>";
-                message += days + " jour" + ( days == 1 ? '':'s' ) + ", ";
-                message += hours + " heure" + ( hours==1 ? '':'s' ) + ", ";
-                message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " et ";
-                message += seconds + " seconde" + ( seconds==1 ? '':'s' );
-                message += "</center>";
-
-
-                $('#tournamentCountdown').html(message);
-            }
-        });
-
-    }
-
+        if (ts > new Date()) {
+            $(this).countdown({
+                timestamp: ts
+            });
+        }
+    });
 });
