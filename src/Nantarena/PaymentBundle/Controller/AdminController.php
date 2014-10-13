eb3751e0ef8e01b7b80abb1ccfe4e7593a3378f3
GUIHAL Maxime <sgp.jaconil@gmail.com>
2014-10-13 22:53:00 +0200

    Add admin payment role

--- src/Nantarena/PaymentBundle/Controller/AdminController.php
+++ src/Nantarena/PaymentBundle/Controller/AdminController.php
@@ -16,27 +16,27 @@ class AdminController extends Controller implements DashboardInterface
             'module_links' => array(
                 $translator->trans('payment.admin.dashboard.payment_management') => array(
                     'url' => $this->generateUrl('nantarena_admin_payment_payment_list'),
-                    'role' => 'ROLE_ADMIN'
+                    'role' => 'ROLE_PAYMENT_ADMIN'
                 ),
                 $translator->trans('payment.admin.dashboard.transaction_management') => array(
                     'url' => $this->generateUrl('nantarena_admin_payment_transaction_list'),
-                    'role' => 'ROLE_ADMIN'
+                    'role' => 'ROLE_PAYMENT_ADMIN'
                 ),
                 $translator->trans('payment.admin.dashboard.paypal_management') => array(
                     'url' => $this->generateUrl('nantarena_admin_payment_paypal_list'),
-                    'role' => 'ROLE_ADMIN'
+                    'role' => 'ROLE_PAYMENT_ADMIN'
                 ),
                 $translator->trans('payment.admin.dashboard.refund_management') => array(
                     'url' => $this->generateUrl('nantarena_admin_payment_refund_list'),
-                    'role' => 'ROLE_ADMIN'
+                    'role' => 'ROLE_PAYMENT_ADMIN'
                 ),
                 $translator->trans('payment.admin.dashboard.user_management') => array(
                     'url' => $this->generateUrl('nantarena_admin_payment_user_list'),
-                    'role' => 'ROLE_ADMIN'
+                    'role' => 'ROLE_PAYMENT_ADMIN'
                 ),
                 $translator->trans('payment.admin.dashboard.coupons_management') => array(
                     'url' => $this->generateUrl('nantarena_admin_payment_coupons_list'),
-                    'role' => 'ROLE_ADMIN'
+                    'role' => 'ROLE_PAYMENT_ADMIN'
                 ),
             )
         );
