8b58406469a2247d729334ca073973abe62f55f2
GUIHAL Maxime <sgp.jaconil@gmail.com>
2014-10-26 20:03:43 +0100

    add admin payment page

--- src/Nantarena/PaymentBundle/Controller/Admin/CashController.php
+++ src/Nantarena/PaymentBundle/Controller/Admin/CashController.php
@@ -2,6 +2,8 @@
 
 namespace Nantarena\PaymentBundle\Controller\Admin;
 
+use Nantarena\PaymentBundle\Entity\Transaction;
+use Nantarena\PaymentBundle\Form\Type\PaymentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 // Manage routing
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -9,16 +11,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 // Request for form
 use Symfony\Component\HttpFoundation\Request;
 
-
 // Entity
 use Nantarena\EventBundle\Entity\Event;
 use Nantarena\EventBundle\Entity\Entry;
 
 use Nantarena\PaymentBundle\Entity\Payment;
 use Nantarena\PaymentBundle\Entity\CashPayment;
-
-use Nantarena\UserBundle\Entity\User;
-
 
 /**
  * Class CashController
@@ -30,13 +28,50 @@ use Nantarena\UserBundle\Entity\User;
 class CashController extends Controller
 {
     /**
-     * @Route("/user/{id}", name="nantarena_admin_payment_cash_user")
+     * @Route("/entry/{id}", name="nantarena_admin_payment_cash_user")
      * @Template()
      */
-    public function payUserAction(User $user)
+    public function payUserAction(Request $request, Entry $entry)
     {
+        $form = $this->createFormBuilder(array('entry_id' => $entry->getId()))
+            ->add('entry_id', 'hidden')
+            ->add('submit', 'submit')
+            ->setMethod('POST')
+            ->setAction($this->generateUrl('nantarena_admin_payment_cash_user', array(
+                'id' => $entry->getId()
+            )))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $transaction = new Transaction();
+            $transaction->setUser($entry->getUser());
+            $transaction->setEvent($entry->getTournament()->getEvent());
+            $transaction->setPrice(0);
+
+            $payment = new CashPayment();
+            $payment->setUser($entry->getUser());
+            $payment->setAdmin($this->getUser());
+            $payment->addTransaction($transaction);
+            $payment->setDate(new \DateTime());
+            $payment->setValid(true);
+
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($transaction);
+            $em->persist($payment);
+            $em->flush();
+
+            $flashbag = $this->get('session')->getFlashBag();
+            $translator = $this->get('translator');
+
+            $flashbag->add('success', $translator->trans('payment.admin.payment.cash.success'));
+            return $this->redirect($this->generateUrl('nantarena_event_admin_entries', array('slug' => $entry->getTournament()->getEvent()->getSlug())));
+        }
+
         return array(
-        	'user' => $user,
+        	'entry' => $entry,
+            'form' => $form->createView()
         );
     }
 }
